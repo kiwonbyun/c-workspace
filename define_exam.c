a2750d787d654a5e82af067cbfaea63f05ae7803
kiwonbyun <bkw9603@naver.com>
2024-03-16 14:21:23 +0900

    선행처리기 정의수정

--- define_exam.c
+++ define_exam.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #define ADDR "서울시 종로구 대학로 86"
 #define TEL "01099999999"
-#define Sum1 (x,y) x+y
-#define Sum2 (x,y) ((x)+(y)) 
-#define Mutiple1 (x,y) x*y
-#define Mutiple2 (x,y) ((x)*(y))
+#define Sum1(x,y) (x+y)
+#define Sum2(x,y) ((x)+(y)) 
+#define Mutiple1(x,y) (x*y)
+#define Mutiple2(x,y) ((x)*(y))
 
 int main(){
   printf("한국방송통신대학교 주소: %s\n", ADDR);
--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-char a[] = "Computer";
-  // char b[] = "Science";
-  // puts(a);
-  // puts(b);
-  // printf("%s", a);
-  // printf("%s", b);
